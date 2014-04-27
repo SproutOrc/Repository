6a8634aca768304705275942d66353a5d6b1f4c1
Atropos <atropos@AtroposdeMacBook-Pro.local>
2014-04-27 15:58:39 +0800

    Initial Commit

---
+++ Control/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Control
+//
+//  Created by Atropos on 14-4-27.
+//  Copyright (c) 2014年 Atropos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Control/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Control
+//
+//  Created by Atropos on 14-4-27.
+//  Copyright (c) 2014年 Atropos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
